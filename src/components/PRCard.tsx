0352d96a733661002044c548683cbeeec8ea05b7
Jonatan Tiago <jonatan2206@gmail.com>
2022-12-12 11:54:10 -0300

    White-space break

--- src/components/PRCard.tsx
+++ src/components/PRCard.tsx
@@ -12,7 +12,7 @@ const PRCard = (props: Props): JSX.Element => {
         <div className="pr-card">
             <h3 className="title"><a href={url} target="_blank" className="link" rel="noreferrer">{title}</a></h3>
             <p className="author">By <img src={authorAvatar} alt="Author avatar" className="avatar" /> {author}</p>
-            <p>{description}</p>
+            <p className="description">{description}</p>
             <p>Last updated: {moment(lastUpdate).format('DD/MM/YYYY')}</p>
         </div>
     );
